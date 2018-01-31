1ab69519dc58a360474b097f04fe2a3028c45d4a
Bhupal <bhupal4all@users.noreply.github.com>
2018-01-31 16:35:18 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAtD9yHaP1cDdH5ha3_q0gOnYWw6DSjnYo",
+    authDomain: "fir-curd-b6e55.firebaseapp.com",
+    databaseURL: "https://fir-curd-b6e55.firebaseio.com",
+    projectId: "fir-curd-b6e55",
+    storageBucket: "fir-curd-b6e55.appspot.com",
+    messagingSenderId: "994806490266"
+  }
 };
